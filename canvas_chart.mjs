229980278f704182b0ba744a7b6f899b054bd95d
Horimatsu Takuya <new_woory@hotmail.com>
2019-07-27 11:13:00 +0900

    fix

--- canvas_chart.mjs
+++ canvas_chart.mjs
@@ -13,8 +13,13 @@ function chooseAxisValues(axis, data, splitNum) {
 }
 
 class CanvasChart {
-  constructor(elm) {
+  constructor(elm, config = {}) {
     this.canvas = elm;
+    this.offsetX = config["offsetX"] || 30.5;
+    this.offsetY = config["offsetY"] || 10.5;
+    this.offsetWidth = config["offsetWidth"] || 20;
+    this.offsetHeight = config["offsetHeight"] || 120;
+    this.axisNum = config["axisNum"] || 10;
   }
 
   paint(values, axis) {
@@ -22,18 +27,19 @@ class CanvasChart {
       w: this.canvas.width, h: this.canvas.height
     };
 
-    let ctx = this.canvas.getContext('2d');
+    const ctx = this.canvas.getContext('2d');
     ctx.lineWidth = 0.5;
-    const offsetX = 30.5;
-    const offsetY = 10.5;
-    const offsetWidth = 20;
-    const offsetHeight = 120;
+    const offsetX = this.offsetX;
+    const offsetY = this.offsetY;
+    const offsetWidth = this.offsetWidth;
+    const offsetHeight = this.offsetHeight;
     const graphWidth = canObj.w - offsetWidth - offsetX;
     const graphHeight = canObj.h - offsetY - offsetHeight;
     const zeroPointX = offsetX + 1;
     const zeroPointXY = canObj.h  - offsetHeight;
-    const axisNum = 10;
+    const axisNum = this.axisNum;
     const axisRotate = 75 / 180 * Math.PI;
+    const xAxisGap = graphWidth / axisNum;
     console.log("graphWidth", graphWidth, "graphHeight", graphHeight);
     ctx.strokeRect(offsetX, offsetY, canObj.w - offsetX - offsetWidth, canObj.h - offsetY - offsetHeight);
     ctx.stroke();
@@ -45,6 +51,20 @@ class CanvasChart {
     // console.log(axisValues);
     console.log("min", min, "max", max);
 
+    // Axis line
+    ctx.lineWidth = 0.5;
+    ctx.strokeStyle = '#7777777f';
+    for (let i = 0;i < axisNum;i++) {
+      let x = xAxisGap * i + offsetX;
+      ctx.beginPath();
+      ctx.moveTo(x, offsetY);
+      ctx.lineTo(x,offsetY + graphHeight);
+      ctx.stroke();
+    }
+
+
+    // Paint main chart
+    ctx.strokeStyle = 'deeppink';
     ctx.beginPath();
 
     ctx.translate(offsetX + 1, offsetY + graphHeight);
@@ -58,23 +78,22 @@ class CanvasChart {
     }
     ctx.stroke();
 
+    // Paint Vertical Axis
     ctx.fillStyle = '#000';
     ctx.font = '12px Sans-Serif';
 
     ctx.scale(1, -1);
-    ctx.fillText(max.toString(), -20, offsetY - graphHeight);
-    ctx.fillText(min.toString(), -20, offsetY);
+    ctx.fillText(max.toString(), -offsetX, offsetY - graphHeight);
+    ctx.fillText(min.toString(), -offsetX, offsetY);
     ctx.translate(-offsetX - 1, -offsetY - graphHeight);
 
 
+    // Paint Horizontal axis
     ctx.fillStyle = '#000';
     ctx.font = '12px Sans-Serif';
     ctx.textBaseline = 'hanging';
 
-
     ctx.translate(zeroPointX - 5, zeroPointXY);
-    let txtWidth = 0;
-    let diff = graphWidth / axisNum;
     for (let i = 0;i <= axisNum;i++) {
       ctx.rotate(-axisRotate);
       let txt = "" + axisValues[i];
@@ -82,7 +101,7 @@ class CanvasChart {
 
       ctx.fillText(txt, -txtWidth, 0);
       ctx.rotate(axisRotate);
-      ctx.translate(diff, 0);
+      ctx.translate(xAxisGap, 0);
     }
   }
 }
